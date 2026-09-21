d1cd587deb5fb1df6672defcbcec06f5bf4d92f4
idowuemmanuel913-arch <idowuemmanuel913@gmail.com>
2026-09-21 07:28:13 +0100

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,1 +1,31 @@
+import os
+import requests
 
+TOKEN = os.environ["WHATSAPP_TOKEN"]
+PHONE_NUMBER_ID = os.environ["PHONE_NUMBER_ID"]
+
+def send_message(number, message):
+    url = f"https://graph.facebook.com/v23.0/{PHONE_NUMBER_ID}/messages"
+
+    headers = {
+        "Authorization": f"Bearer {TOKEN}",
+        "Content-Type": "application/json"
+    }
+
+    data = {
+        "messaging_product": "whatsapp",
+        "to": number,
+        "type": "text",
+        "text": {
+            "body": message
+        }
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+
+    print(response.status_code)
+    print(response.text)
+
+
+# Example:
+# send_message("2348012345678", "Hello from Shinsuke's bot 👑")
